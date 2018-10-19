7506d7d4de0d46c7049b2d15dbae9a3f0760f4fb
Artem Ulko <ulkoart@gmail.com>
2018-10-19 14:57:25 +0300

    add pylint & docstring

--- django_masterpass/apps.py
+++ django_masterpass/apps.py
@@ -1,5 +1,13 @@
+"""
+as part of integration into the general list of applications
+"""
+
 from django.apps import AppConfig
 
 
 class DjangoMasterpassConfig(AppConfig):
+    """
+    the config is set
+    """
+
     name = 'django_masterpass'
--- django_masterpass/auth.py
+++ django_masterpass/auth.py
@@ -1,3 +1,7 @@
+"""
+main module
+"""
+
 import warnings
 
 from django.contrib.auth.hashers import check_password, make_password
@@ -5,10 +9,13 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
-UserModel = get_user_model()
+USER_MODEL = get_user_model()
 
 
 class MasterPass(ModelBackend):
+    """
+    MasterPass authentication backend
+    """
     def __init__(self):
         try:
             self.master_pass = getattr(settings, 'MASTER_PASS')
@@ -19,19 +26,18 @@ class MasterPass(ModelBackend):
 
     def __getattribute__(self, name):
         if name == 'authenticate' and not self.master_pass:
-            return
+            return None
         return super(MasterPass, self).__getattribute__(name)
 
     def authenticate(self, request, username=None, password=None, **kwargs):
         if not self.master_pass:
             return None
         if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD)
+            username = kwargs.get(USER_MODEL.USERNAME_FIELD)
         try:
-            user = UserModel._default_manager.get_by_natural_key(username)
-        except UserModel.DoesNotExist:
-            return
+            user = USER_MODEL._default_manager.get_by_natural_key(username)
+        except USER_MODEL.DoesNotExist:
+            return None
         if check_password(password, make_password(self.master_pass)):
             return user
-        else:
-            return
+        return None
